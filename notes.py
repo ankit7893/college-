0d6262fa871ad3cedbf73a72e5f2985326ae3882
ankit <ankit_home@ankits-MacBook-Air.local>
2022-06-08 17:41:37 +0530

    second commit

---
+++ notes.py
@@ -0,0 +1,10 @@
+# first time ----- 
+# git init 
+# git add . 
+# git commit -m "first commit"
+# repo create 
+# git remote add origin https://github.com/ankit7893/college-.git
+# git branch -M main 
+# git push -u origin main
+
+
