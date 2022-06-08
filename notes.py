2cf92a007c0b307930f1f0692781494b28b7dabf
ankit <ankit_home@ankits-MacBook-Air.local>
2022-06-08 21:17:58 +0530

    changes made in notes.py

--- notes.py
+++ notes.py
@@ -1,4 +1,5 @@
-# first time ----- 
+# first time -----  
+
 # git init 
 # git add . 
 # git commit -m "first commit"
@@ -6,5 +7,13 @@
 # git remote add origin https://github.com/ankit7893/college-.git
 # git branch -M main 
 # git push -u origin main
+##########################################
+
+
+# second time ---  changes
+
+# git add . 
+# git commit -m "second commit"
+# git push origin main 
 
 
